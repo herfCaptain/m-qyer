87c4a235591344509cb6034d9214c6461cb9d6e7
captain <callwild@aliyun.com>
2018-10-21 23:02:45 +0800

    ac

--- src/scripts/controlllers/home.js
+++ src/scripts/controlllers/home.js
@@ -1,6 +1,12 @@
 import homeTpl from "../views/home.html"
 const render = () => {
 	document.querySelector("#root").innerHTML=homeTpl;
+	if(localStorage.userNow){
+		$(".userinfo>a>span").text("ID:"+localStorage.userNow)
+	}else{
+		$(".userinfo>a>span").text("登录")
+	}
+	
 	//scroll();
 	
 }
